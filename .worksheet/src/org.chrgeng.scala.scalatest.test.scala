f8cb61fcfe6eb3b4131e13e1e337f72b61d3db0e
Geng, Christian (415) <christian.geng@daimler.com>
2017-09-27 12:24:09 +0100

    initial commit

---
+++ .worksheet/src/org.chrgeng.scala.scalatest.test.scala
@@ -0,0 +1,6 @@
+package org.chrgeng.scala.scalatest
+
+object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(94); 
+  println("Welcome to the Scala worksheet");$skip(15); 
+  println("s")}
+}
---
+++ src/main/scala/org/chrgeng/scala/scalatest/test.sc
@@ -0,0 +1,6 @@
+package org.chrgeng.scala.scalatest
+
+object test {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  println("s")                                    //> s
+}
